889e43aa0dd955cafecf1027e813a01b8da3145c
PixelFireblade <joshuamoses@outlook.com>
2017-03-15 17:42:20 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 
 $ ( document ).ready(function(){
-  if ($('#choiceOptionHistory').click()){
-    $('#randomOptionHistory').prop('checked', false);
-   }
+  $('#choiceOptionHistory').change(changeOptions);
+  $('#randomOptionHistory').change(changeOptions2);
 });
   
+function changeOptions () {
+  
+  //When Choice is selected
+  if ($('#choiceOptionHistory').prop('checked', true)){
+    
+    //Deselects Random
+    $('#randomOptionHistory').prop('checked', false);
+    $('#choiceOptionHistory').prop('checked', true);
+   
+    
+    
+  }
+  function changeOptions2 () {
+  
+  //When Random is selected
+  if ($('#randomOptionHistory').prop('checked', true)){
+    
+    //Deselects Choice
+    $('#choiceOptionHistory').prop('checked', false);
+    $('#randomOptionHistory').prop('checked', true);
+   
+    
+    
+  }
