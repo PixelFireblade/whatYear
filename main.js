d28bb39ea40c343747a7b06579ca927090c6882e
PixelFireblade <joshuamoses@outlook.com>
2017-03-15 17:05:07 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 $ ( document ).ready(function(){
-  if ($('#choiceOptionHistory').click(function(){
+  if ($('#choiceOptionHistory').click(function()){
     $('#randomOptionHistory').prop('checked', false);
-   })
+   }
 });
   
