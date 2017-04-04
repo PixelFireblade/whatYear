01c57ed4d6ff9cac79963e6d7338eca0aeddb6c2
PixelFireblade <joshuamoses@outlook.com>
2017-04-04 14:04:50 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var randomNumber = Math.floor(Math.random() * (3016 - 1 + 1)) + 1;
+if randomNumber > 1000 {
+  radomNumber = randomNumber + "BC"
+}
 console.log(randomNumber);
 $ ( document ).ready(function(){
   $('#choiceOptionHistory').change(changeOptions);
