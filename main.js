a0b40223a6b9d1eea5a4264e6372145af116c874
PixelFireblade <joshuamoses@outlook.com>
2017-05-02 13:41:25 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,25 @@ $ ( document ).ready(function(){
   $('#choiceOptionHistory').change(changeOptions);
   $('#randomOptionHistory').change(changeOptions2);
   $("#findYear").click(function() {
+    if ($('#choiceOptionHistory').prop('checked', true)){
+        var year = $('#choiceOptionHistory2');
+        var year = parseInt(year);
+        var year = year + " " + $('#bcAD');
+    }
     
-    var year = Math.floor(Math.random() * (2017 - 1 + 1)) + 1;
-    var year = year + " AD"
+    else if ($('#randomOptionHistory').prop('checked', true)) {
+      var year = Math.floor(Math.random() * (3017 - 1 + 1)) + 1;
+      if(year > 1017) {
+        var year - year - 1000;
+        var year = year + " AD";
+      }
+      else {
+        var year = year + " BC"
+      }
+      
+      
+    }
+    
     
     console.log(year);
     
